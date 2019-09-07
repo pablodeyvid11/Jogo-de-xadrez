99532a2ea54c0d4673f2eaaf1bf16033d88f34db
Pablo Deyvid de Paiva <pablodeyvid11@gmail.com>
2019-09-07 10:32:14 -0300

    Primeira classe: posicao

--- src/application/program.java
+++ src/application/program.java
@@ -1,9 +1,13 @@
 package application;
 
+import tabuleiro.posicao;
+
 public class program {
 
 	public static void main(String[] args) {
+		posicao pos = new posicao(2, 3);
 		
+		System.out.println(pos);
 	}
 
 }
