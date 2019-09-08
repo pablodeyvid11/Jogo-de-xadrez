43c8a026f1f62e65bc5ec4f2418ad0ca455842e8
Pablo Deyvid de Paiva <pablodeyvid11@gmail.com>
2019-09-08 12:13:00 -0300

    Tratando excessoes e limpando a tela

--- src/application/UI.java
+++ src/application/UI.java
@@ -30,16 +30,22 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void LimparTela() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
 	public static xadrezPosicao lerPosicaoXadrez(Scanner sc) {
 		try {
 			String s = sc.nextLine().trim();
 			char coluna = s.charAt(0);
 			int linha = Integer.parseInt(s.substring(1));
 			return new xadrezPosicao(coluna, linha);
-		} catch(RuntimeException e) {
+		} catch (RuntimeException e) {
 			throw new InputMismatchException("Erro ao iniciar a posição, valores validos são: de a1 até h8");
 		}
-		
+
 	}
 
 	public static void escreverTabuleiro(pecaXadrez[][] pecaxadr) {
--- src/application/program.java
+++ src/application/program.java
@@ -1,9 +1,11 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import xadresCamada.partidaXadrez;
 import xadresCamada.pecaXadrez;
+import xadresCamada.xadrezExcessao;
 import xadresCamada.xadrezPosicao;
 
 public class program {
@@ -11,19 +13,30 @@ public class program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		partidaXadrez partida = new partidaXadrez();
-		
+
 		while (true) {
-			UI.escreverTabuleiro(partida.getPecasXadrez());
-			System.out.println();
-			System.out.print("Digite a posição de origem: ");
-			xadrezPosicao origem = UI.lerPosicaoXadrez(sc);
-			
-			System.out.println();
-			System.out.print("Digite a posição de destino: ");
-			xadrezPosicao destino = UI.lerPosicaoXadrez(sc);
-			
-			pecaXadrez pecaCapturada = partida.executarMovimento(origem, destino);
+			try {
+				UI.LimparTela();
+				UI.escreverTabuleiro(partida.getPecasXadrez());
+				System.out.println();
+				System.out.print("Digite a posição de origem: ");
+				xadrezPosicao origem = UI.lerPosicaoXadrez(sc);
+
+				System.out.println();
+				System.out.print("Digite a posição de destino: ");
+				xadrezPosicao destino = UI.lerPosicaoXadrez(sc);
+
+				pecaXadrez pecaCapturada = partida.executarMovimento(origem, destino);
+			} catch (xadrezExcessao e) {
+				System.out.println(e.getMessage());
+				System.out.println("Aperte enter para continuar:");
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				System.out.println("Aperte enter para continuar:");
+				sc.nextLine();
+
+			}
 		}
 	}
-
 }
